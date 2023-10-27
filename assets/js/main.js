6927640af89bd7f0405b1c01aab6b5dcab839bf3
freetimezp <kraftukrnet@ukr.net>
2023-10-27 21:57:17 +0300

    15. js-services-15-books. Active menu on scroll.

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,7 +113,26 @@ const scrollUp = () => {
 window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
 
+const scrollActive = () => {
+    const scrollDown = window.scrollY;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute('id');
+        const sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionClass.classList.add('active-link');
+        } else {
+            sectionClass.classList.remove('active-link');
+        }
+    });
+};
+
+window.addEventListener('scroll', scrollActive);
 
 /*=============== DARK LIGHT THEME ===============*/
 
